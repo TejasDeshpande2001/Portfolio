96095f529fe03eeb9774a577af7fe84110c2f6c5
killertej <tejasdeshpande29@gmail.com>
2024-07-08 15:48:49 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+// import React from 'react'
+import {HERO_CONTENT} from "../constants"
+import profilePic from "../assets/kevinRushProfile.png"
+
+const Hero = () => {
+    return (
+        <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+            <div className="flex flex-wrap">
+                <div className="w-full lg:w-1/2">
+                    <div className="flex flex-col items-center lg:items-start">
+                        <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+                            Tejas Deshpande
+                        </h1>
+                        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                            Full Stack Developer
+                        </span>
+                        <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+                            {HERO_CONTENT}
+                        </p>
+                    </div>
+                </div>
+
+                <div className="w-full lg:w-1/2 lg:p-8">
+                    <div className="flex justify-center">
+                        <img src="https://images.unsplash.com/photo-1534665482403-a909d0d97c67?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Tejas" />
+                    </div>
+                </div>
+
+
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Hero
---
+++ src/constants/index.js
@@ -0,0 +1,58 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a  full stack developer with a knack for crafting robust and scalable web applications. With 3 years of hands-on experience, I have honed my skills in front-end technologies like Html , Css , JavaScript And React , as well as back-end technologies like  MySQL. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 2-3 years of hand on experience, I have worked with a variety of technologies, including React, MySQL, Python. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and learn new things all time.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "MCA - at JSPM RSCOE ,PUNE ",
+    // company: "Google Inc.",
+  },
+  {
+    year: "2020 - 2023",
+    role: "BCA - from Punyashlok Ahilyadevi Holkar University, Solapur",
+    company: "9.01 CGPA",
+  },
+  {
+    year: "2018 - 2020",
+    role: "12'th from Shri Shivaji Mahavidyalaya, Barshi",
+    company: "64%",
+  },
+  {
+    year: "2018",
+    role: "10'th Sulakhe High school Barshi",
+    company: "79%",
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website  (2018-2019)",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS","Bootstrap", "ASP.NET","MySql" ],
+  },
+  {
+    title: "Product Management System (2022-2023)",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "JS", "PHP"],
+  },
+  {
+    title: "Portfolio Website (2024)",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap","Tailwind"],
+  },
+ 
+];
+
+
